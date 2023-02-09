1a80e71b321ac5cc3755d9d77190e985cf1a73d3
maks0nch1k <maksimf647@gmail.com>
2023-02-09 22:25:20 +0300

    updated classes.py

--- classes.py
+++ classes.py
@@ -28,18 +28,27 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(constants.PLAYER_GROUP, constants.ALL_SPRITES)
         self.image = constants.PLAYER_IMAGE
-        self.rect = self.image.get_rect().move(
-            constants.TILE_WIDTH * pos_x, constants.TILE_HEIGHT * pos_y)
+        self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.bg_now = 0
+        self.x = pos_x
+        self.pos_y = pos_y
+        self.transparent = (0, 0, 0, 0)
+        self.v = 100
+
+    def update(self):
+        self.image = pygame.transform.rotate(self.image, 10)
+        self.rect = self.image.get_rect().move(self.x, self.pos_y)
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, x):
-        super().__init__(constants.ALL_SPRITES)
+        super().__init__(constants.BACKGROUND_GROUP, constants.ALL_SPRITES)
         self.image = constants.TILE_IMAGES["background"]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.x = x
         self.v = 100
+        print(self.rect)
 
     def update(self):
         self.x -= self.v / constants.FPS
